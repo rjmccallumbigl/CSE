b1cb1981fc77a082eb36bf23f0c7b1aac5d0bb77
Ryan McCallum <15747450+rjmccallumbigl@users.noreply.github.com>
2024-09-27 15:45:16 -0400

    Update badscript.sh

--- badscript.sh
+++ badscript.sh
@@ -3,7 +3,6 @@
 echo "Starting script...";
 myParameter=$(cat /etc/fstab);
 getResult=$($myParam | grep "nofail");
-nonexistent_command "$getResult";
 echo "Command finished"
 echo "Result is $getResult"
 echo "Script complete!"
